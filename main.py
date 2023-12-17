ca9567e126e1500e8ef14f67adba69839c9b93d0
Vladimir Semenenko <wsemenenko@gmail.com>
2023-12-17 14:54:37 +0300

    Fix folders destination

--- main.py
+++ main.py
@@ -4,12 +4,6 @@ from datetime import date
 
 from config import Config
 from parsers.screendata import parse_main, parse_info
-import logging
-
-
-logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG, filename='log.txt', filemode='w')
-logging.getLogger(__name__).setLevel(logging.DEBUG)
 
 
 def main():
