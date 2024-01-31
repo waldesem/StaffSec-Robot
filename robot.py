e20de5570b416ddec3a2e686172b566096b0cb81
Vladimir Semenenko <wsemenenko@gmail.com>
2024-01-31 20:47:35 +0300

    Fix issues

--- robot.py
+++ robot.py
@@ -72,30 +72,31 @@ async def parse_main():
                 and (c.value).date() == date.today()
             ):
                 fio = fullname_parser(ws["B" + str(i)].value)
-                subdir = [sub for sub in subdirs if fullname_parser(sub) == fio]
-                if subdir:
-                    subdir_path = os.path.join(Config.WORK_DIR, subdir)
+                for sub in subdirs:
+                    if fullname_parser(sub) == fio:
+                        subdir_path = os.path.join(Config.WORK_DIR, sub)
 
-                    for file in os.listdir(subdir_path):
-                        if (file.startswith("Заключение") or file.startswith("Результаты")) and (
-                            file.endswith("xlsm") or file.endswith("xlsx")
-                        ):
-                            await excel_to_db(subdir_path, file)
-                        elif file.endswith("json"):
-                            await json_to_db(subdir_path, file)
+                        for file in os.listdir(subdir_path):
+                            if (file.startswith("Заключение") or file.startswith("Результаты")) and (
+                                file.endswith("xlsm") or file.endswith("xlsx")
+                            ):
+                                await excel_to_db(subdir_path, file)
+                            elif file.endswith("json"):
+                                await json_to_db(subdir_path, file)
 
-                    lnk = os.path.join(
-                        Config.ARCHIVE_DIR, subdir[0], f"{subdir} - {ws['A' + str(i)].value}"
-                    )
-                    ws["L" + str(i)].hyperlink = str(lnk)
-                    try:
-                        shutil.move(subdir_path, lnk)
-                        logging.info(f"{subdir_path} moved to {lnk}")
-                    except Exception as e:
-                        logging.error(e)
+                        lnk = os.path.join(
+                            Config.ARCHIVE_DIR, sub[0], f"{sub} - {ws['A' + str(i)].value}"
+                        )
+                        ws["L" + str(i)].hyperlink = str(lnk)
+                        try:
+                            shutil.move(subdir_path, lnk)
+                            logging.info(f"{subdir_path} moved to {lnk}")
+                        except Exception as e:
+                            logging.error(e)
+                        
+                        break
                 
                 await (screen_registry_data(ws, i))
-
 
     wb.save(Config.MAIN_FILE)
     wb.close()
